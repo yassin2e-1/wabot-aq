cae4c6781e97f0c553cdfa55096ad34ff9100432
yassin2e-1 <84070941+yassin2e-1@users.noreply.github.com>
2021-08-24 09:48:01 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-global.owner = ['+212642061003'] // Put your number here
+global.owner = ['212642061003'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
